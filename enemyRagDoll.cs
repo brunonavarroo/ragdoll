c57812602891bfb2f4525266d4bb2a9c35ec3f2b
flamencoBusiness <53520681+flamencoBusiness@users.noreply.github.com>
2020-07-04 18:11:52 +0200

    Add files via upload

---
+++ enemyRagDoll.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyRagDoll : MonoBehaviour
+{
+    [SerializeField] Animator enemyAnimator;
+
+    private void Start()
+    {
+        SetRigidbodyState(true);
+        SetColliderState(false);
+    }
+    public void death()
+    {
+        enemyAnimator.enabled = false;
+        SetRigidbodyState(false);
+        SetColliderState(true);
+    }
+
+    void SetRigidbodyState(bool state)
+    {
+
+        Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody rigidbody in rigidbodies)
+        {
+            rigidbody.isKinematic = state;
+        }
+
+        GetComponent<Rigidbody>().isKinematic = state; // previously !state
+
+    }
+
+    void SetColliderState(bool state)
+    {
+
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = state;
+        }
+        GetComponent<Collider>().enabled = !state;
+
+    }
+}
